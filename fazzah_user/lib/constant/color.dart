ac6bb64c9c7cbefc941a67f0d2ffe23547251c72
Rahaf Alqahtani <86989761+rahafwmaq@users.noreply.github.com>
2023-12-18 12:59:23 +0300

    add the constan file contain the colors layout

---
+++ fazzah_user/lib/constant/color.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+const Color white = Color(0xffffffff);
+const Color black = Color.fromARGB(255, 0, 0, 0);
+const Color noColor = Color.fromARGB(0, 204, 107, 100);
+const Color red = Color(0xffFF3B30);
+const Color grey = Color(0xffB4B9C0);
+const Color lightGrey = Color(0xffE1E1E1);
+const Color green = Color(0xff2C5602);
+const Color lightGreen = Color(0xffEFF0EB);
+const Color coldGreen = Color(0xffAAF3B1);
+const Color dark1Green = Color(0xff152604);
+const Color dark2Green = Color(0xff1F2916);
---
+++ fazzah_user/lib/utils/extentions/get_first_word.dart
@@ -0,0 +1,5 @@
+// function retirn the first name 
+
+String getFirstWords(String sentence) {
+  return sentence.split(" ").first;
+}
