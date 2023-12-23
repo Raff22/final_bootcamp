a1ea39d57e44f856b1fa49deab90c1136457c793
mac <mac@macs-MacBook-Pro.local>
2023-12-23 17:22:31 +0300

    upddated

--- fazzah_user/lib/main.dart
+++ fazzah_user/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:fazzah_user/utils/helpers/supabase_initilizer.dart';
+import 'package:fazzah_user/views/auth_views/login_view/login_view.dart';
 import 'package:fazzah_user/views/chat_views/chat_with_provider_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-  supaInitializer();
+  // await dotenv.load(fileName: ".env");
+  // supaInitializer();
   await EasyLocalization.ensureInitialized();
 
   runApp(
@@ -31,6 +32,6 @@ class MainApp extends StatelessWidget {
         localizationsDelegates: context.localizationDelegates,
         supportedLocales: context.supportedLocales,
         locale: context.locale,
-        home: const ChatWithProviderView());
+        home: const LoginView());
   }
 }
