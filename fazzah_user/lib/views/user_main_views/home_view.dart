c75a7b9a9b26249caeaeb16ffc0828db6cff4eaa
unknown <dr.fisal99@gmail.com>
2023-12-23 16:07:41 +0300

    sd

---
+++ fazzah_user/lib/views/user_main_views/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MainView extends StatelessWidget {
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> list = [
+      "assets/images/AD.png",
+      "assets/images/AD-2.png",
+      "assets/images/AD-3.png",
+    ];
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
