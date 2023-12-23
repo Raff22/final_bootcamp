0eebad1b1865090f2584957241bd78a01af40917
mac <mac@macs-MacBook-Pro.local>
2023-12-23 15:56:41 +0300

    added .env

--- fazzah_user/lib/main.dart
+++ fazzah_user/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:fazzah_user/views/chat_views/chat_with_provider_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await EasyLocalization.ensureInitialized();
 
   runApp(
